dbcf5cd457073cd1ddfd5caa170a39b39bedcb6b
Joon Park <jpark1@grubhub.com>
2020-05-03 17:47:23 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect, useRef } from 'react';
+import { Oscillator, Panner, Master } from 'tone';
+import styled from 'styled-components';
 import './App.css';
 
+const ButtonWrapper = styled.div`
+  display: flex;
+`;
+
+function useOscillator(pitch, type, pan) {
+  const osc = useRef(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  useEffect(() => {
+    osc.current = new Oscillator(pitch, type).chain(new Panner(pan), Master);
+  }, []);
+
+  function togglePlaying() {
+    if (isPlaying) {
+      osc.current.stop();
+    } else {
+      osc.current.start();
+    }
+    setIsPlaying(!isPlaying);
+  }
+
+  return [isPlaying, togglePlaying, osc.current];
+}
+
 function App() {
+  const [inTunePitchIsPlaying, toggleInTunePitch] = useOscillator(440, 'triangle', -1);
+  const [outOfTunePitchIsPlaying, toggleOutOfTunePitch] = useOscillator(450, 'sine', 1);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <ButtonWrapper>
+        <button onClick={toggleInTunePitch}>
+          {inTunePitchIsPlaying ? 'Stop' : 'Play'}
+        </button>
+        <button onClick={toggleOutOfTunePitch}>
+          {outOfTunePitchIsPlaying ? 'Stop' : 'Play'}
+        </button>
+      </ButtonWrapper>
+    </main>
   );
 }
 
